0536fe2dec9dbe74bef140b93d9a1ef46f7d4a20
neerajkashetty <neeraj.kasheety29@gmail.com>
2024-07-07 21:53:31 -0400

    add-splash-screen-to-the-application

--- src/index.js
+++ src/index.js
@@ -1,40 +1,59 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
 } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Home from "./pages/HomePage";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Splash from "./pages/Splash";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { RecoilRoot } from "recoil";
+
+function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {showSplash ? (
+        <Splash />
+      ) : (
+        <Router>
+          <RecoilRoot>
+            <Routes>
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route
+                path="/home/:conversationId?"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+          </RecoilRoot>
+        </Router>
+      )}
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Router>
-    <RecoilRoot>
-      <Routes>
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-
-        <Route
-          path="/home/:conversationId?"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        ></Route>
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </RecoilRoot>
-  </Router>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
