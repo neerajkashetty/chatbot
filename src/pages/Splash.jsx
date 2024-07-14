31e4c2e54dbda7123b58c8a4863324822c320f75
neerajkashetty <neeraj.kasheety29@gmail.com>
2024-07-14 12:20:41 -0400

    fix: splash-screen

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,7 +1,7 @@
 
 function Splash() {
   return (
-    <div id="canvas-container" className="w-screen h-screen bg-black flex justify-center">
+    <div id="canvas-container" className="w-screen h-screen bg-white flex justify-center">
     <video autoPlay muted loop >
       <source src="/uc.mp4" type="video/mp4"/>
     </video>
